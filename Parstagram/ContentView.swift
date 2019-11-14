07a957635faedc90e62edd0e55e63f660d934e34
Kelly von Borstel <kellyvonborstel@gmail.com>
2019-11-14 02:52:16 -0800

    Initial Commit

---
+++ Parstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parstagram
+//
+//  Created by Kelly von Borstel on 11/14/19.
+//  Copyright © 2019 kellyvonborstel@gmail.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
